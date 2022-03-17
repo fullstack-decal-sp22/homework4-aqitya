0d006211640253a390744605919650430b2fcdf4
Aditya Tummala <adi.tummala@berkeley.edu>
2022-03-17 15:26:18 -0700

    Updates

---
+++ precache-manifest.9c5376fb26fb02c850ff9fc268a97e6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cff50d444042092c65e7",
+    "url": "/homework4-%7Byour_username%7D/static/css/main.fc84dda1.chunk.css"
+  },
+  {
+    "revision": "cff50d444042092c65e7",
+    "url": "/homework4-%7Byour_username%7D/static/js/main.cff50d44.chunk.js"
+  },
+  {
+    "revision": "24c69f000997ca075680",
+    "url": "/homework4-%7Byour_username%7D/static/js/1.24c69f00.chunk.js"
+  },
+  {
+    "revision": "67898790825aa8cf4efd",
+    "url": "/homework4-%7Byour_username%7D/static/js/runtime~main.67898790.js"
+  },
+  {
+    "revision": "0b8a24f64c1170fdade45394ac46a3c6",
+    "url": "/homework4-%7Byour_username%7D/index.html"
+  }
+];
