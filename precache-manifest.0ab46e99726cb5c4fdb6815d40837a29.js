376321d36b7dbd0c23cc1d63b50853b7cf441373
Aditya Tummala <adi.tummala@berkeley.edu>
2022-03-20 18:56:55 -0700

    Updates

---
+++ precache-manifest.0ab46e99726cb5c4fdb6815d40837a29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74f0198d6594ba37321a",
+    "url": "/homework4-aqitya/static/css/main.ef09413d.chunk.css"
+  },
+  {
+    "revision": "74f0198d6594ba37321a",
+    "url": "/homework4-aqitya/static/js/main.74f0198d.chunk.js"
+  },
+  {
+    "revision": "4a41ce067e05bf3f2f18",
+    "url": "/homework4-aqitya/static/js/1.4a41ce06.chunk.js"
+  },
+  {
+    "revision": "d98ba8b4a656a1e23f14",
+    "url": "/homework4-aqitya/static/js/runtime~main.d98ba8b4.js"
+  },
+  {
+    "revision": "4f47b531ec53c566bf2d1d74d2572512",
+    "url": "/homework4-aqitya/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/homework4-%7Byour_username%7D/precache-manifest.27f1d989c9575892962aa83d9f101e05.js"
+  "/homework4-aqitya/precache-manifest.0ab46e99726cb5c4fdb6815d40837a29.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/homework4-%7Byour_username%7D/index.html", {
+workbox.routing.registerNavigationRoute("/homework4-aqitya/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
